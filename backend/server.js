c8b65d128f9201838dd0df1254b9fa0cc8d7699b
Саша <vishnya3d@gmail.com>
2026-04-25 11:05:57 +0000

    fix: add reconnect to mysql

--- backend/server.js
+++ backend/server.js
@@ -43,7 +43,8 @@ const db = mysql.createConnection({
   user: process.env.MYSQLUSER,
   password: process.env.MYSQLPASSWORD,
   database: process.env.MYSQLDATABASE,
-  port: process.env.MYSQLPORT
+  port: process.env.MYSQLPORT,
+  reconnect: true
 });
 
 db.connect((err) => {
